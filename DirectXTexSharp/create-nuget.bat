dcb469aacfe362cbec2886bb15a9ee6d9c16f4ee
rfuzzo <r.fuzzo@gmail.com>
2021-07-11 18:04:39 +0200

    Update create-nuget.bat

--- DirectXTexSharp/create-nuget.bat
+++ DirectXTexSharp/create-nuget.bat
@@ -2,7 +2,7 @@
 
 echo "nuget pack x64"
 
-nuget pack .\DirectXTexSharp.nuspec -Build -NoPackageAnalysis -Properties 'Configuration=Debug;Platform=x64;CConfiguration=Debug;CPlatform=x64;' -OutputDirectory nuget -Version 0.8
+nuget pack .\DirectXTexSharp.nuspec -Build -NoPackageAnalysis -Properties 'Configuration=Debug;Platform=x64;CConfiguration=Debug;CPlatform=x64;' -OutputDirectory nuget -Version 0.2
 
 ::echo "nuget pack win32"
 ::nuget pack .\DirectXTexSharp.nuspec -Build -NoPackageAnalysis -Properties 'Configuration=Release;Platform=win32;CConfiguration=Release;CPlatform=win32;' -OutputDirectory nuget -Version 0.4
